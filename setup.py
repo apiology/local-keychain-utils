99477ab5942feb9b6a3f1cd5ceeb6911c13999e6
Vince Broz <vince@broz.cc>
2018-08-12 16:00:51 -0400

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/apiology/local_keychain_utils',
-    version='1.0.2',
+    version='1.0.3',
     zip_safe=False,
     cmdclass={
         'quality': QualityCommand,
