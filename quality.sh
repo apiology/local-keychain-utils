a9a498d74b77f05bb37ccd1dabab59c3bd5aaac4
Vince Broz <vince@broz.cc>
2018-08-12 17:21:14 -0400

    Save bundle for quality run

--- quality.sh
+++ quality.sh
@@ -1,25 +1,13 @@
 #!/bin/bash -x
 
-bundle install
-type gem
-type bundle
-
-bundle exec gem list quality
-
-
-if bundle exec gem list quality -i >/dev/null 2>&1
+if ! bundle exec gem list quality -i >/dev/null 2>&1
 then
-  if [ -f Rakefile.quality ]
-  then
-    rake -f Rakefile.quality quality
-  else
-    rake quality
-  fi
+  bundle install
+fi
+
+if [ -f Rakefile.quality ]
+then
+  bundle exec rake -f Rakefile.quality quality
 else
-  if [ -f Rakefile.quality ]
-  then
-    docker run -v "$(pwd)":/usr/app -v "$(pwd)"/Rakefile.quality:/usr/quality/Rakefile apiology/quality:jumbo-latest
-  else
-    docker run -v "$(pwd):/usr/app" apiology/quality:jumbo-latest "$@"
-  fi
+  bundle exec rake quality
 fi
