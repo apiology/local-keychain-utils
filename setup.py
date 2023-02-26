26d1cc973ec2a5f7194c4a78589a787aa2db22de
Vince Broz <vince@broz.cc>
2023-02-26 17:58:33 -0500

    Update template

--- setup.py
+++ setup.py
@@ -97,6 +97,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     description="CLI tools to manipulate local keychain",  # noqa: E501
     entry_points={
